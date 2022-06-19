a26f65dfabfa0177869c0306a77bfc9c8f931e82
shogo61 <58659726+shogo61@users.noreply.github.com>
2022-06-19 15:34:41 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,19 +10,24 @@ public class Main{
         int[] nums=new int[N];
         int cnt=0;
         while(cnt<N){
+            // 一度testに乱数を生成
             int test=rd.nextInt(9)+1;
             int flag=0;
+            
+            // nums内にtestが格納されているか調べる
             for(int j=0;j<N;j++){
                 if(test==nums[j]){
                     flag=1;
                 }
             }
+            // 格納されていなければnumsに格納して次のインデックスに移動
             if(flag==0){
                 nums[cnt]=test;
                 cnt+=1;
             }
         }
-
+        
+        // 答えの表示
         for(int i=0;i<nums.length;i++){
             System.out.print(nums[i]+" ");
         }
